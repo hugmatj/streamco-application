e0e38f17bfcc215895cb38a96b27a0f8e1b32a31
gareth stokes <gareth.stokes@fairfaxmedia.com.au>
2014-05-21 21:20:50 +1000

    drm post route with bound DrmParams

--- server.go
+++ server.go
@@ -2,13 +2,52 @@ package main
 
 import(
   "github.com/go-martini/martini"
+  "github.com/martini-contrib/render"
+  "github.com/martini-contrib/binding"
+
 )
+
+type Show struct {
+  Image         interface{}  `json:"image"`
+  Slug          interface{}  `json:"slug"`
+  Title         interface{}  `json:"title"`
+  Drm           bool    `json:"drm"`
+  EpisodeCount  int     `json:"episodeCount"`
+}
+
+type DrmParams struct {
+  Payload       []Show    `json:"payload"`
+  Skip          int       `json:"skip"`
+  Take          int       `json:"take"`
+  TotalRecords  int       `json:"totalRecords"`
+}
+
+func ok(r render.Render, shows []Show) {
+  r.JSON(200, map[string] []Show { "response": shows })
+}
+
+func err(r render.Render, message string) {
+  r.JSON(400, map[string] string { "error": message })
+}
 
 func main() {
   m := martini.Classic()
 
-  m.Get("/", func() string {
-    return "Hello world!"
+  m.Use(render.Renderer(render.Options{
+    Charset: "UTF-8", // Sets encoding for json and html content-types. Default is "UTF-8".
+    IndentJSON: true, // Output human readable JSON
+  }))
+
+  m.Post("/drm", binding.Bind(DrmParams{}), func(params DrmParams, r render.Render) {
+    valid := make([]Show, 0)
+
+    for _, show := range params.Payload {
+      if show.Drm && show.EpisodeCount > 0 {
+        valid = append(valid, show)
+      }
+    }
+
+    ok(r, valid)
   })
 
   m.Run()
